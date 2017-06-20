4b9b00cc00baf734390f604c709e97a39452b6c7
EthanChappel <EthanChappel@users.noreply.github.com>
2017-06-19 22:59:44 -0500

    Add Orthographic projection of Earth to location dialog

--- py3/modifylocation.py
+++ py3/modifylocation.py
@@ -1,39 +1,92 @@
 import os
 import json
-from PyQt5 import QtWidgets
+from typing import List, Tuple
+from PyQt5 import QtCore, QtWidgets
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import ui_modifylocation
 
 
 class LocationDialog(QtWidgets.QDialog):
     def __init__(self):
         super(LocationDialog, self).__init__()
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
         try:
             with open("location.json", "r") as f:
                 self.location = json.load(f)
         except (FileNotFoundError, json.decoder.JSONDecodeError):
             self.location = {"Latitude": "00:00:00", "Longitude": "00:00:00"}
         self.lat = self.location["Latitude"].split(":")
-        self.long = self.location["Longitude"].split(":")
-        self.ui = ui_modifylocation.Ui_LocationDialog()
+        self.lon = self.location["Longitude"].split(":")
+        self.dec_lat, self.dec_lon = self.decimal_coordinates([int(i) for i in self.lat], [int(i) for i in self.lon])
 
+        self.earth_figure = plt.figure(facecolor=(0.333, 0.333, 0.333))
+        self.earth_canvas = FigureCanvas(self.earth_figure)
+
+        self.ui = ui_modifylocation.Ui_LocationDialog()
         self.ui.setupUi(self)
         self.setup_gui()
 
     def setup_gui(self):
         self.ui.lat_d_spin.setValue(int(self.lat[0]))
-        self.ui.long_d_spin.setValue(int(self.long[0]))
+        self.ui.long_d_spin.setValue(int(self.lon[0]))
         self.ui.lat_m_spin.setValue(int(self.lat[1]))
-        self.ui.long_m_spin.setValue(int(self.long[1]))
+        self.ui.long_m_spin.setValue(int(self.lon[1]))
         self.ui.lat_s_spin.setValue(int(self.lat[2]))
-        self.ui.long_s_spin.setValue(int(self.long[2]))
+        self.ui.long_s_spin.setValue(int(self.lon[2]))
+
+        self.ui.lat_d_spin.valueChanged.connect(lambda: self.generate_map())
+        self.ui.long_d_spin.valueChanged.connect(lambda: self.generate_map())
+        self.ui.lat_m_spin.valueChanged.connect(lambda: self.generate_map())
+        self.ui.long_m_spin.valueChanged.connect(lambda: self.generate_map())
+        self.ui.lat_s_spin.valueChanged.connect(lambda: self.generate_map())
+        self.ui.long_s_spin.valueChanged.connect(lambda: self.generate_map())
         self.ui.button_box.accepted.connect(self.ok)
         self.ui.button_box.rejected.connect(self.cancel)
 
-    # Do if "OK" button is pressed
+        self.generate_map()
+        self.ui.earth_layout.addWidget(self.earth_canvas)
+
+    def decimal_coordinates(self, lat: List[int], lon: List[int]) -> Tuple[float, float]:
+        """Convert coordinates in ###:##:## format to floats."""
+        if int(lat[0]) < 0:
+            lat_dec = int(lat[0]) + (-1 * (float(lat[1]) / 60)) + (-1 * (float(lat[2]) / 3600))
+        else:
+            lat_dec = int(lat[0]) + (float(lat[1]) / 60) + (float(lat[2]) / 3600)
+        if int(lon[0]) < 0:
+            lon_dec = int(lon[0]) + (-1 * (float(lon[1]) / 60)) + (-1 * (float(lon[2]) / 3600))
+        else:
+            lon_dec = int(lon[0]) + (float(lon[1]) / 60) + (float(lon[2]) / 3600)
+        return lat_dec, lon_dec
+
+    def generate_map(self):
+        """Generate map to display in window."""
+        # TODO: Fix map shrinking on first change
+        self.earth_figure.clf()
+        self.lat = [self.ui.lat_d_spin.value(), self.ui.lat_m_spin.value(), self.ui.lat_s_spin.value()]
+        self.lon = [self.ui.long_d_spin.value(), self.ui.long_m_spin.value(), self.ui.long_s_spin.value()]
+        self.dec_lat, self.dec_lon = self.decimal_coordinates([int(i) for i in self.lat], [int(i) for i in self.lon])
+        earth_axes = plt.axes(projection=ccrs.Orthographic(self.dec_lon, self.dec_lat))
+        earth_axes.add_feature(cfeature.LAND, facecolor="white")
+        earth_axes.add_feature(cfeature.LAKES, edgecolor="black", facecolor="white")
+        earth_axes.add_feature(cfeature.RIVERS, edgecolor="black")
+        earth_axes.set_global()
+        earth_axes.plot(self.dec_lon, self.dec_lat, 'ro', markersize=4)
+        # earth_axes.add_feature(cfeature.OCEAN, facecolor="white")
+        earth_axes.add_feature(cfeature.COASTLINE)
+        self.earth_figure.add_subplot(earth_axes)
+        self.earth_figure.tight_layout()
+        self.earth_canvas.draw()
+
     def ok(self):
+        """Do if "OK" button is pressed"""
         self.accept()
-        self.latitude = str(self.ui.lat_d_spin.value()) + ":" + str(self.ui.lat_m_spin.value()) + ":" + str(self.ui.lat_s_spin.value())
-        self.longitude = str(self.ui.long_d_spin.value()) + ":" + str(self.ui.long_m_spin.value()) + ":" + str(self.ui.long_s_spin.value())
+        self.latitude = str(self.ui.lat_d_spin.value()) + ":" + str(self.ui.lat_m_spin.value()) + ":" + str(
+            self.ui.lat_s_spin.value())
+        self.longitude = str(self.ui.long_d_spin.value()) + ":" + str(self.ui.long_m_spin.value()) + ":" + str(
+            self.ui.long_s_spin.value())
 
         if os.path.exists("location.json"):
             os.remove("location.json")
@@ -42,7 +95,12 @@ class LocationDialog(QtWidgets.QDialog):
         location_dict.update({"Longitude": self.longitude})
         with open("location.json", "a") as f:
             json.dump(location_dict, f, indent=0)
+        self.earth_figure.clf()
 
-    # Do if "Cancel" button is pressed
     def cancel(self):
+        """Do if "Cancel" button is pressed"""
         self.reject()
+        self.earth_figure.clf()
+
+    def closeEvent(self, event):
+        self.earth_figure.clf()
--- py3/ui_modifylocation.py
+++ py3/ui_modifylocation.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'modifylocation.ui'
 #
-# Created: Sun Apr 16 00:21:09 2017
-#      by: PyQt5 UI code generator 5.3.2
+# Created: Mon Jun 19 21:50:37 2017
+#      by: PyQt5 UI code generator 5.2.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,13 +12,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_LocationDialog(object):
     def setupUi(self, LocationDialog):
         LocationDialog.setObjectName("LocationDialog")
-        LocationDialog.resize(318, 79)
+        LocationDialog.resize(257, 333)
+        LocationDialog.setMinimumSize(QtCore.QSize(257, 333))
+        LocationDialog.setMaximumSize(QtCore.QSize(257, 333))
         self.verticalLayout = QtWidgets.QVBoxLayout(LocationDialog)
         self.verticalLayout.setContentsMargins(0, 0, 0, -1)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.earth_proj = QtWidgets.QWidget(LocationDialog)
-        self.earth_proj.setObjectName("earth_proj")
-        self.verticalLayout.addWidget(self.earth_proj)
+        self.earth_layout = QtWidgets.QHBoxLayout()
+        self.earth_layout.setObjectName("earth_layout")
+        self.verticalLayout.addLayout(self.earth_layout)
         self.gridLayout_2 = QtWidgets.QGridLayout()
         self.gridLayout_2.setContentsMargins(9, -1, 9, -1)
         self.gridLayout_2.setObjectName("gridLayout_2")
@@ -27,28 +29,24 @@ class Ui_LocationDialog(object):
         self.button_box.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.button_box.setObjectName("button_box")
         self.gridLayout_2.addWidget(self.button_box, 0, 6, 2, 1)
-        self.long_label = QtWidgets.QLabel(LocationDialog)
-        self.long_label.setObjectName("long_label")
-        self.gridLayout_2.addWidget(self.long_label, 1, 0, 1, 1)
         self.lat_label = QtWidgets.QLabel(LocationDialog)
         self.lat_label.setObjectName("lat_label")
         self.gridLayout_2.addWidget(self.lat_label, 0, 0, 1, 1)
+        self.lat_m_spin = QtWidgets.QSpinBox(LocationDialog)
+        self.lat_m_spin.setMaximum(59)
+        self.lat_m_spin.setObjectName("lat_m_spin")
+        self.gridLayout_2.addWidget(self.lat_m_spin, 0, 2, 1, 1)
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_2.addItem(spacerItem, 0, 4, 2, 1)
+        self.long_label = QtWidgets.QLabel(LocationDialog)
+        self.long_label.setObjectName("long_label")
+        self.gridLayout_2.addWidget(self.long_label, 1, 0, 1, 1)
         self.line = QtWidgets.QFrame(LocationDialog)
         self.line.setFrameShadow(QtWidgets.QFrame.Plain)
         self.line.setFrameShape(QtWidgets.QFrame.VLine)
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
         self.gridLayout_2.addWidget(self.line, 0, 5, 2, 1)
-        self.long_m_spin = QtWidgets.QSpinBox(LocationDialog)
-        self.long_m_spin.setMaximum(59)
-        self.long_m_spin.setObjectName("long_m_spin")
-        self.gridLayout_2.addWidget(self.long_m_spin, 1, 2, 1, 1)
-        self.lat_m_spin = QtWidgets.QSpinBox(LocationDialog)
-        self.lat_m_spin.setMaximum(59)
-        self.lat_m_spin.setObjectName("lat_m_spin")
-        self.gridLayout_2.addWidget(self.lat_m_spin, 0, 2, 1, 1)
         self.long_s_spin = QtWidgets.QSpinBox(LocationDialog)
         self.long_s_spin.setMaximum(59)
         self.long_s_spin.setObjectName("long_s_spin")
@@ -58,13 +56,17 @@ class Ui_LocationDialog(object):
         self.lat_s_spin.setObjectName("lat_s_spin")
         self.gridLayout_2.addWidget(self.lat_s_spin, 0, 3, 1, 1)
         self.lat_d_spin = QtWidgets.QSpinBox(LocationDialog)
-        self.lat_d_spin.setMinimum(-90)
-        self.lat_d_spin.setMaximum(90)
+        self.lat_d_spin.setMinimum(-89)
+        self.lat_d_spin.setMaximum(89)
         self.lat_d_spin.setObjectName("lat_d_spin")
         self.gridLayout_2.addWidget(self.lat_d_spin, 0, 1, 1, 1)
+        self.long_m_spin = QtWidgets.QSpinBox(LocationDialog)
+        self.long_m_spin.setMaximum(59)
+        self.long_m_spin.setObjectName("long_m_spin")
+        self.gridLayout_2.addWidget(self.long_m_spin, 1, 2, 1, 1)
         self.long_d_spin = QtWidgets.QSpinBox(LocationDialog)
-        self.long_d_spin.setMinimum(-180)
-        self.long_d_spin.setMaximum(180)
+        self.long_d_spin.setMinimum(-179)
+        self.long_d_spin.setMaximum(179)
         self.long_d_spin.setObjectName("long_d_spin")
         self.gridLayout_2.addWidget(self.long_d_spin, 1, 1, 1, 1)
         self.verticalLayout.addLayout(self.gridLayout_2)
@@ -77,12 +79,12 @@ class Ui_LocationDialog(object):
     def retranslateUi(self, LocationDialog):
         _translate = QtCore.QCoreApplication.translate
         LocationDialog.setWindowTitle(_translate("LocationDialog", "Solar System Sequencer - Location"))
-        self.long_label.setText(_translate("LocationDialog", "Longitude"))
         self.lat_label.setText(_translate("LocationDialog", "Latitude"))
-        self.long_m_spin.setSuffix(_translate("LocationDialog", "m"))
         self.lat_m_spin.setSuffix(_translate("LocationDialog", "m"))
+        self.long_label.setText(_translate("LocationDialog", "Longitude"))
         self.long_s_spin.setSuffix(_translate("LocationDialog", "s"))
         self.lat_s_spin.setSuffix(_translate("LocationDialog", "s"))
         self.lat_d_spin.setSuffix(_translate("LocationDialog", "°"))
+        self.long_m_spin.setSuffix(_translate("LocationDialog", "m"))
         self.long_d_spin.setSuffix(_translate("LocationDialog", "°"))
 

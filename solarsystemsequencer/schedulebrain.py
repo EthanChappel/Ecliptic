7ff57a4a689d31767af25f78a5a2526bee90cc9c
EthanChappel <EthanChappel@users.noreply.github.com>
2017-08-17 02:22:34 -0500

    Implement altitude and directional preference parameters

--- solarsystemsequencer/schedulebrain.py
+++ solarsystemsequencer/schedulebrain.py
@@ -1,7 +1,6 @@
 import math
-from typing import Tuple
+from typing import List
 from PyQt5 import QtCore, QtWidgets
-import ephem
 import pandas as pd
 import appglobals
 from computetargets import ComputeTargets
@@ -18,23 +17,28 @@ class ScheduleBrain(QtWidgets.QDialog, ui_schedulebrain.Ui_ScheduleBrainDialog):
         self.date = date
         self.exec_()
 
-    def generate_schedule(self, start: str="00:00", end: str="23:59", max_sun_ele: int=-18, elevation=None,
-                          preference=None):
+    def generate_schedule(self, targets: List[str], start: str="00:00", end: str="23:59",
+                          max_sun_ele: int=-18, min_ele: int=12, max_ele: int=90, preference: str="Both"):
         time_range = list(pd.date_range(self.date+" "+start, self.date+" "+end, freq="60min").to_pydatetime())
 
-        for t in reversed(time_range):
-            targets = ComputeTargets(t, appglobals.location["Latitude"], appglobals.location["Longitude"])
-            sun_alt = targets.object_alt("Sun")["alt"] * 180 / math.pi
+        for r in reversed(time_range):
+            compute = ComputeTargets(r, appglobals.location["Latitude"], appglobals.location["Longitude"])
+            sun_alt = compute.object_alt("Sun")["alt"] * 180 / math.pi
             if sun_alt > max_sun_ele:
-                time_range.remove(t)
+                time_range.remove(r)
 
-        if elevation is not None:
-            pass
+            # Limit altitude of targets
+            candidates = [t for t in targets if min_ele <= compute.object_alt(t)["alt"] * 180 / math.pi <= max_ele]
 
-        if preference is not None:
-            pass
+            # Stay on one side of meridian if set
+            if preference == "East":
+                candidates = [t for t in candidates if 0 <= compute.object_alt(t)["az"] * 180 / math.pi < 180]
+            elif preference == "West":
+                candidates = [t for t in candidates if 180 <= compute.object_alt(t)["az"] * 180 / math.pi < 360]
 
     def ok(self):
-        self.generate_schedule(start=self.start_timeedit.text(), end=self.end_timeedit.text(),
-                               max_sun_ele=int(self.sunelevation_spinbox.cleanText()))
+        self.generate_schedule(targets=[t.text() for t in self.targets_buttongroup.buttons() if t.isChecked()],
+                               start=self.start_timeedit.text(), end=self.end_timeedit.text(),
+                               max_sun_ele=int(self.sunelevation_spinbox.cleanText()),
+                               preference=self.directional_combobox.currentText())
         self.accept()

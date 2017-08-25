20ed0c1fb65fd13fe463932f7734d2b65e4fb187
EthanChappel <EthanChappel@users.noreply.github.com>
2017-08-25 01:55:56 -0500

    Implement schedule generation if looking for highest target

--- solarsystemsequencer/schedulebrain.py
+++ solarsystemsequencer/schedulebrain.py
@@ -1,4 +1,5 @@
 import math
+import datetime
 from typing import List
 from PyQt5 import QtCore, QtWidgets
 import pandas as pd
@@ -17,16 +18,22 @@ class ScheduleBrain(QtWidgets.QDialog, ui_schedulebrain.Ui_ScheduleBrainDialog):
         self.date = date
         self.exec_()
 
-    def generate_schedule(self, targets: List[str], start: str="00:00", end: str="23:59",
-                          max_sun_ele: int=-18, min_ele: int=12, max_ele: int=90, preference: str="Both"):
-        time_range = list(pd.date_range(self.date+" "+start, self.date+" "+end, freq="60min").to_pydatetime())
+    def generate_schedule(self, targets: List[str], start_time: str, end_time: str, max_sun_ele: int, min_ele: int,
+                          max_ele: int, interval: int, preference: str, target_pref: str, no_target_action: str,
+                          end_action: str):
 
+        time_range = list(pd.date_range(self.date + " " + start_time, self.date + " " + end_time,
+                                        freq=str(interval)+"min").to_pydatetime())
+
+        schedule = {}
         for r in reversed(time_range):
             compute = ComputeTargets(r, appglobals.location["Latitude"], appglobals.location["Longitude"])
             sun_alt = compute.object_alt("Sun")["alt"] * 180 / math.pi
             if sun_alt > max_sun_ele:
                 time_range.remove(r)
 
+        for r in time_range:
+            compute = ComputeTargets(r, appglobals.location["Latitude"], appglobals.location["Longitude"])
             # Limit altitude of targets
             candidates = [t for t in targets if min_ele <= compute.object_alt(t)["alt"] * 180 / math.pi <= max_ele]
 
@@ -35,10 +42,50 @@ class ScheduleBrain(QtWidgets.QDialog, ui_schedulebrain.Ui_ScheduleBrainDialog):
                 candidates = [t for t in candidates if 0 <= compute.object_alt(t)["az"] * 180 / math.pi < 180]
             elif preference == "West":
                 candidates = [t for t in candidates if 180 <= compute.object_alt(t)["az"] * 180 / math.pi < 360]
+            schedule[r] = candidates
+
+        final = {}
+        prev = None
+        # TODO: Meridian flips
+        for l in schedule:
+            compute = ComputeTargets(l, appglobals.location["Latitude"], appglobals.location["Longitude"])
+            target = None
+            if target_pref == "Less for longer":
+                lowest = max_ele
+                for t in schedule.get(l):
+                    if min_ele < compute.object_alt(t)["alt"] * 180 / math.pi < lowest:
+                        pass
+                        '''target = t
+                        lowest = compute.object_alt(t)["alt"] * 180 / math.pi'''
+            elif target_pref == "Prefer highest":
+                highest = min_ele
+                for t in schedule.get(l):
+                    if max_ele > compute.object_alt(t)["alt"] * 180 / math.pi > highest:
+                        target = t
+                        highest = compute.object_alt(t)["alt"] * 180 / math.pi
+                else:
+                    if target is None and highest == min_ele:
+                        target = no_target_action
+            # Add to final if it wasn't previously added
+            if target != prev:
+                print(l, target)
+                final[l] = target
+                prev = target
+        date = [int(i) for i in self.date.split("/")]
+        time = [int(i) for i in end_time.split(":")]
+        final[datetime.datetime(date[0], date[1], date[2], time[0], time[1])] = end_action
+        # TODO: Save to schedule.json and reload it
 
     def ok(self):
         self.generate_schedule(targets=[t.text() for t in self.targets_buttongroup.buttons() if t.isChecked()],
-                               start=self.start_timeedit.text(), end=self.end_timeedit.text(),
-                               max_sun_ele=int(self.sunelevation_spinbox.cleanText()),
-                               preference=self.directional_combobox.currentText())
+                               start_time=self.start_timeedit.text(),
+                               end_time=self.end_timeedit.time().toString(),
+                               max_sun_ele=self.sunelevation_spinbox.value(),
+                               min_ele=self.minelevation_spinbox.value(),
+                               max_ele=self.maxelevation_spinbox.value(),
+                               interval=self.interval_spinbox.value(),
+                               preference=self.directional_combobox.currentText(),
+                               target_pref=self.targetpreference_combobox.currentText(),
+                               no_target_action=self.notarget_combobox.currentText(),
+                               end_action=self.endaction_combobox.currentText())
         self.accept()

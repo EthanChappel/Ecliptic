c46bfc4a9623da7b17a10a1e3b7aab894e381826
EthanChappel <EthanChappel@users.noreply.github.com>
2019-01-15 03:11:28 -0600

    Merge __main__ and guistyle.

--- solarsystemsequencer/__main__.py
+++ solarsystemsequencer/__main__.py
@@ -2,8 +2,7 @@
 import sys
 import traceback
 from typing import Any
-from PyQt5 import QtCore, QtWidgets
-from guistyle import switch_style
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 sys.dont_write_bytecode = True
 
@@ -27,7 +26,33 @@ def excepthook(exc_type: tuple, exc_val: Any, tracebackobj: traceback):
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 app = QtWidgets.QApplication(sys.argv)
 
-switch_style(app, True)
+palette = QtGui.QPalette()
+palette.setColor(QtGui.QPalette.Window, QtGui.QColor(85, 85, 85))
+palette.setColor(QtGui.QPalette.Button, QtGui.QColor(95, 95, 95))
+palette.setColor(QtGui.QPalette.Foreground, QtGui.QColor(255, 255, 255))
+palette.setColor(QtGui.QPalette.ButtonText, QtGui.QColor(255, 255, 255))
+palette.setColor(QtGui.QPalette.Light, QtGui.QColor(110, 110, 110))
+palette.setColor(QtGui.QPalette.Dark, QtGui.QColor(75, 75, 75))
+palette.setColor(QtGui.QPalette.Text, QtGui.QColor(255, 255, 255))
+palette.setColor(QtGui.QPalette.Link, QtGui.QColor(255, 50, 50))
+palette.setColor(QtGui.QPalette.Base, QtGui.QColor(95, 95, 95))
+palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(200, 80, 80))
+palette.setColor(QtGui.QPalette.HighlightedText, QtGui.QColor(255, 255, 255))
+
+palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Button, QtGui.QColor(75, 75, 75))
+palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Text, QtGui.QColor(200, 200, 200))
+palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, QtGui.QColor(200, 200, 200))
+palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Highlight, QtGui.QColor(80, 80, 80))
+palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Foreground, QtGui.QColor(200, 200, 200))
+
+app.setStyleSheet("""
+    QDockWidget {
+       titlebar-normal-icon: url(:/icons/ic_open_in_new_white_48px.svg);
+       titlebar-close-icon: url(:/icons/ic_close_white_48px.svg);
+    }
+""")
+app.setPalette(palette)
+app.setStyle("Fusion")
 
 sys.excepthook = excepthook
 import mainwindow

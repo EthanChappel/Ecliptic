50fe8e5206ef2d14b946079c4d1ec03c0a5ed6c6
EthanChappel <EthanChappel@users.noreply.github.com>
2020-02-07 11:35:23 -0600

    Rename "device" to "driver"

--- solarsystemsequencer/equipment/ascom.py
+++ solarsystemsequencer/equipment/ascom.py
@@ -15,14 +15,14 @@ class AscomDevice(Device):
     _device_type = None
 
     def __init__(self, device: type):
-        self.device = device(self.chooser(type(self)._device_type))
+        self.driver = device(self.chooser(type(self)._device_type))
         self.connect()
 
     def device_name(self) -> str:
-        return self.device.Name
+        return self.driver.Name
 
     def set_connected(self, connected: bool):
-        self.device.Connected = connected
+        self.driver.Connected = connected
 
     def connect(self):
         self.set_connected(True)
@@ -31,14 +31,14 @@ class AscomDevice(Device):
         self.set_connected(False)
 
     def is_connected(self) -> bool:
-        return self.device.Connected
+        return self.driver.Connected
 
     def dispose(self):
-        self.device.Dispose()
-        del self.device
+        self.driver.Dispose()
+        del self.driver
 
     def setup_dialog(self):
-        self.device.SetupDialog()
+        self.driver.SetupDialog()
 
     @staticmethod
     def chooser(device_type: str) -> str:
@@ -54,43 +54,43 @@ class AscomTelescope(Telescope, AscomDevice):
         super().__init__(ASCOM.DriverAccess.Telescope)
 
     def can_slew_eq(self) -> bool:
-        return self.device.CanSlew
+        return self.driver.CanSlew
 
     def can_slew_alt_az(self) -> bool:
-        return self.device.CanSlewAltAz
+        return self.driver.CanSlewAltAz
 
     def goto_home(self):
-        self.device.Unpark()
-        self.device.FindHome()
+        self.driver.Unpark()
+        self.driver.FindHome()
 
     def set_parked(self, parked: bool):
         if parked:
-            self.device.Park()
+            self.driver.Park()
         else:
-            self.device.Unpark()
+            self.driver.Unpark()
 
     def is_tracking(self) -> bool:
-        return self.device.Tracking
+        return self.driver.Tracking
 
     def set_tracking(self, tracking: bool):
-        self.device.Tracking = tracking
+        self.driver.Tracking = tracking
 
     def can_slew(self) -> bool:
-        return self.device.CanSlew
+        return self.driver.CanSlew
 
     def goto(self, ra, dec):
-        self.device.Tracking = True
-        self.device.SlewToCoordinates(ra, dec)
+        self.driver.Tracking = True
+        self.driver.SlewToCoordinates(ra, dec)
 
     def move_axis(self, axis: int, rate: float):
-        self.device.Tracking = True
-        self.device.MoveAxis(axis, rate)
+        self.driver.Tracking = True
+        self.driver.MoveAxis(axis, rate)
 
     def pulse_guide(self, direction: int, duration: int):
-        self.device.PulseGuide(direction, duration)
+        self.driver.PulseGuide(direction, duration)
 
     def get_pier_side(self):
-        return self.device.SideOfPier
+        return self.driver.SideOfPier
 
 
 class AscomCamera(Camera, AscomDevice):
@@ -100,48 +100,48 @@ class AscomCamera(Camera, AscomDevice):
         super().__init__(ASCOM.DriverAccess.Camera)
 
     def min_gain(self) -> int:
-        return self.device.GainMin
+        return self.driver.GainMin
 
     def max_gain(self) -> int:
-        return self.device.GainMax
+        return self.driver.GainMax
 
     def gain(self) -> int:
-        return self.device.Gain
+        return self.driver.Gain
 
     def min_exposure(self) -> float:
-        return self.device.ExposureMin
+        return self.driver.ExposureMin
 
     def max_exposure(self) -> float:
-        return self.device.ExposureMax
+        return self.driver.ExposureMax
 
     def image_width(self) -> int:
-        return self.device.NumX
+        return self.driver.NumX
 
     def image_height(self) -> int:
-        return self.device.NumY
+        return self.driver.NumY
 
     def exposure_complete(self) -> bool:
-        return self.device.ImageReady
+        return self.driver.ImageReady
 
     def capture(self, exposure: float, light: bool) -> np.ndarray:
-        self.device.StartExposure(exposure, light)
-        while not self.device.ImageReady:
+        self.driver.StartExposure(exposure, light)
+        while not self.driver.ImageReady:
             pass
-        image = self.device.ImageArray
-        width, height = self.device.NumX, self.device.NumY
+        image = self.driver.ImageArray
+        width, height = self.driver.NumX, self.driver.NumY
         image = np.asarray(list(image), dtype=np.uint8).reshape(width, height)  # list(image) is slow
         image = np.rot90(image, 1)
         image = np.flipud(image)
         return image
 
     def stop_exposure(self):
-        self.device.StopExposure()
+        self.driver.StopExposure()
 
     def exposure_progress(self) -> int:
-        return self.device.PercentCompleted
+        return self.driver.PercentCompleted
 
     def image_array(self) -> List[int]:
-        return list(self.device.ImageArray)
+        return list(self.driver.ImageArray)
 
 
 class AscomFilterWheel(FilterWheel, AscomDevice):
@@ -151,17 +151,17 @@ class AscomFilterWheel(FilterWheel, AscomDevice):
         super().__init__(ASCOM.DriverAccess.FilterWheel)
 
     def wheel_position(self, pos: int):
-        self.device.Position = pos
+        self.driver.Position = pos
 
     def rotate_wheel(self, text: Union[str, int]):
         try:
-            self.device.Position = text
+            self.driver.Position = text
         except Exception:
             for f in appglobals.filters:
                 if f["Name"] == text:
                     wheel_pos = f["Wheel Position"]
                     try:
-                        self.device.Position = wheel_pos
+                        self.driver.Position = wheel_pos
                         break
                     except Exception:
                         pass
@@ -174,22 +174,22 @@ class AscomFocuser(Focuser, AscomDevice):
         super().__init__(ASCOM.DriverAccess.Focuser)
 
     def set_position(self, pos: int):
-        self.device.Move(pos)
+        self.driver.Move(pos)
 
     def get_position(self) -> int:
-        return self.device.Position
+        return self.driver.Position
 
     def is_abs_position(self) -> bool:
-        return self.device.Absolute
+        return self.driver.Absolute
 
     def max_step(self) -> int:
-        return self.device.MaxStep
+        return self.driver.MaxStep
 
     def is_temp_comp(self) -> bool:
-        return self.device.TempComp
+        return self.driver.TempComp
 
     def has_temp_comp(self) -> bool:
-        return self.device.TempCompAvailable
+        return self.driver.TempCompAvailable
 
     def set_temp_comp(self, val: bool):
-        self.device.TempComp = val
+        self.driver.TempComp = val

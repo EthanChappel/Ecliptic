50485322da413fc03fbf996e824ad87390a88c82
Ethan Chappel <EthanChappel@users.noreply.github.com>
2020-12-16 00:54:09 -0600

    Add more options for solving with ASTAP

--- ecliptic/astrometry/astap.py
+++ ecliptic/astrometry/astap.py
@@ -7,18 +7,22 @@ class AstapSolver(Solver):
     def __init__(self, path: str):
         self.program = Path(path)
     
-    def solve(self, image, ra_h=None, dec_d=None, radius_d=None):
+    def solve(self, image, ra_h=None, dec_d=None, radius_d=None, down_sample=None, debug=False):
         path = Path(image)
         command = [
-            str(self.program), '-wcs', '-annotate',
+            str(self.program), '-wcs',
             '-f', str(path),
         ]
 
-        if ra_h:
+        if ra_h is not None:
             command.extend(['-ra', str(ra_h)])
-        if dec_d:
+        if dec_d is not None:
             command.extend(['-spd', str(dec_d + 90)])
-        if radius_d:
+        if radius_d is not None:
             command.extend(['-r', str(radius_d)])
+        if down_sample is not None:
+            command.extend(['-z', str(down_sample)])
+        if debug == True:
+            command.append('-debug')
         
         subprocess.run(command)
